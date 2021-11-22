6d81256024e6a77231ef84a5b07ad7884052a04c
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:20:55 +0100

    Added FloatNormalizer

---
+++ src/bundle/Serializer/Normalizer/FieldType/FloatNormalizer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\Float\Value as FloatValue;
+use eZ\Publish\SPI\Exception\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class FloatNormalizer implements ValueNormalizerInterface
+{
+    public function normalize(Value $value): ?float
+    {
+        if (!$value instanceof FloatValue) {
+            throw new InvalidArgumentType('$value', FloatValue::class);
+        }
+
+        return $value->value;
+    }
+
+    public function supportsValue(Value $value): bool
+    {
+        return $value instanceof FloatValue;
+    }
+}
---
+++ tests/bundle/Serializer/Normalizer/FieldType/FloatNormalizerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\Float\Value;
+use Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType\FloatNormalizer;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class FloatNormalizerTest extends AbstractValueNormalizerTestCase
+{
+    /**
+     * @dataProvider provideDataForTestNormalize
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function testNormalizer(?float $expected, Value $value): void
+    {
+        $this->testNormalize($expected, $value);
+    }
+
+    /**
+     * @return iterable<array{
+     *  ?float,
+     *  \eZ\Publish\Core\FieldType\Float\Value
+     * }>
+     */
+    public function provideDataForTestNormalize(): iterable
+    {
+        yield [
+            1.10000,
+            new Value(1.10000),
+        ];
+
+        yield [
+            null,
+            new Value(),
+        ];
+    }
+
+    protected function getNormalizer(): ValueNormalizerInterface
+    {
+        return new FloatNormalizer();
+    }
+
+    protected function getValue(): Value
+    {
+        return new Value();
+    }
+}
