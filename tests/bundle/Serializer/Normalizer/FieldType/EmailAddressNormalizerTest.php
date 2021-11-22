320977cc7fd1f56f8e9a33b76e478fb78520c09c
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:19:56 +0100

    Added EmailAddressNormalizer

---
+++ tests/bundle/Serializer/Normalizer/FieldType/EmailAddressNormalizerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\EmailAddress\Value;
+use Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType\EmailAddressNormalizer;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class EmailAddressNormalizerTest extends AbstractValueNormalizerTestCase
+{
+    /**
+     * @dataProvider provideDataForTestNormalize
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function testNormalizer(string $expected, Value $value): void
+    {
+        $this->testNormalize($expected, $value);
+    }
+
+    /**
+     * @return iterable<array{
+     *  string,
+     *  \eZ\Publish\Core\FieldType\EmailAddress\Value
+     * }>
+     */
+    public function provideDataForTestNormalize(): iterable
+    {
+        yield [
+            'foo@link.invalid',
+            new Value('foo@link.invalid'),
+        ];
+
+        yield [
+            '',
+            new Value(),
+        ];
+    }
+
+    protected function getNormalizer(): ValueNormalizerInterface
+    {
+        return new EmailAddressNormalizer();
+    }
+
+    protected function getValue(): Value
+    {
+        return new Value();
+    }
+}
