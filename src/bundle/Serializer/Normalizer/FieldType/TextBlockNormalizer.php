e3cf55a83d865c755820cb73a672a0520a492885
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:27:00 +0100

    Added TextBlockNormalizer

---
+++ src/bundle/Serializer/Normalizer/FieldType/TextBlockNormalizer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\TextBlock\Value as TextBlockValue;
+use eZ\Publish\SPI\Exception\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class TextBlockNormalizer implements ValueNormalizerInterface
+{
+    public function normalize(Value $value): string
+    {
+        if (!$value instanceof TextBlockValue) {
+            throw new InvalidArgumentType('$value', TextBlockValue::class);
+        }
+
+        return $value->text;
+    }
+
+    public function supportsValue(Value $value): bool
+    {
+        return $value instanceof TextBlockValue;
+    }
+}
