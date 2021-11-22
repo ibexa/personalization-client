87545154a4d150d14293eed101aa7c1f8b7104c3
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:30:43 +0100

    Added ValueNormalizerNotFoundException

---
+++ src/lib/Exception/ValueNormalizerNotFoundException.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\PersonalizationClient\Exception;
+
+use eZ\Publish\SPI\FieldType\Value;
+use EzSystems\EzRecommendationClient\Exception\EzRecommendationException;
+use RuntimeException;
+use Throwable;
+
+final class ValueNormalizerNotFoundException extends RuntimeException implements EzRecommendationException
+{
+    public function __construct(Value $value, int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct(
+            sprintf('ValueNormalizer not found for field type value: %s.', get_class($value)),
+            $code,
+            $previous
+        );
+    }
+}
