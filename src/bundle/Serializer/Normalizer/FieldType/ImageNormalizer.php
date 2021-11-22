af6c0f240c6b3506d41db68acf2384e118067b29
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:21:34 +0100

    Added ImageNormalizer

---
+++ src/bundle/Serializer/Normalizer/FieldType/ImageNormalizer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\Image\Value as ImageValue;
+use eZ\Publish\SPI\Exception\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\DestinationValueAwareInterface;
+
+final class ImageNormalizer implements DestinationValueAwareInterface
+{
+    public function normalize(Value $value): ?string
+    {
+        if (!$value instanceof ImageValue) {
+            throw new InvalidArgumentType('$value', ImageValue::class);
+        }
+
+        return $value->uri;
+    }
+
+    public function supportsValue(Value $value): bool
+    {
+        return $value instanceof ImageValue;
+    }
+}
---
+++ tests/bundle/Serializer/Normalizer/FieldType/ImageNormalizerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\Image\Value;
+use Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType\ImageNormalizer;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class ImageNormalizerTest extends AbstractValueNormalizerTestCase
+{
+    /**
+     * @dataProvider provideDataForTestNormalize
+
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function testNormalizer(?string $expected, Value $value): void
+    {
+        $this->testNormalize($expected, $value);
+    }
+
+    /**
+     * @return iterable<array{
+     *  ?string,
+     *  \eZ\Publish\Core\FieldType\Image\Value
+     * }>
+     *
+     * @throws \eZ\Publish\Core\Base\Exceptions\InvalidArgumentType
+     */
+    public function provideDataForTestNormalize(): iterable
+    {
+        yield [
+            'public/var/test/1/2/3/4/5/file.invalid',
+            new Value(
+                [
+                    'id' => 1,
+                    'alternativeText' => 'Test image',
+                    'fileName' => 'file.invalid',
+                    'fileSize' => 123456,
+                    'uri' => 'public/var/test/1/2/3/4/5/file.invalid',
+                    'imageId' => '',
+                    'inputUri' => 'storage/file.invalid',
+                    'width' => 120,
+                    'height' => 80,
+                    'additionalData' => [],
+                ]
+            ),
+        ];
+
+        yield [
+            null,
+            new Value(
+                [
+                    'id' => null,
+                    'alternativeText' => null,
+                    'fileName' => null,
+                    'fileSize' => null,
+                    'uri' => null,
+                    'imageId' => null,
+                    'inputUri' => null,
+                    'width' => null,
+                    'height' => null,
+                    'additionalData' => [],
+                ]
+            ),
+        ];
+
+        yield [
+            null,
+            new Value(),
+        ];
+    }
+
+    protected function getNormalizer(): ValueNormalizerInterface
+    {
+        return new ImageNormalizer();
+    }
+
+    protected function getValue(): Value
+    {
+        return new Value();
+    }
+}
