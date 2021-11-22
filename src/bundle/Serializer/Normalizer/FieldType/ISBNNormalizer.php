54c138e4d77453c4c4e0dc8fafd4ea17af296439
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:23:11 +0100

    Added ISBNNormalizer

---
+++ src/bundle/Serializer/Normalizer/FieldType/ISBNNormalizer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\ISBN\Value as ISBNValue;
+use eZ\Publish\SPI\Exception\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class ISBNNormalizer implements ValueNormalizerInterface
+{
+    public function normalize(Value $value): string
+    {
+        if (!$value instanceof ISBNValue) {
+            throw new InvalidArgumentType('$value', ISBNValue::class);
+        }
+
+        return $value->isbn;
+    }
+
+    public function supportsValue(Value $value): bool
+    {
+        return $value instanceof ISBNValue;
+    }
+}
