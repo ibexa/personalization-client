f97fe87edcf2ea7968ed07e29fdf17f42b45aaff
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:28:15 +0100

    Added UrlNormalizer

---
+++ tests/bundle/Serializer/Normalizer/FieldType/UrlNormalizerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\Url\Value;
+use Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType\UrlNormalizer;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class UrlNormalizerTest extends AbstractValueNormalizerTestCase
+{
+    /**
+     * @dataProvider provideDataForTestNormalize
+     *
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function testNormalizer(?string $expected, Value $value): void
+    {
+        $this->testNormalize($expected, $value);
+    }
+
+    /**
+     * @return iterable<array{
+     *  ?string,
+     *  \eZ\Publish\Core\FieldType\Url\Value
+     * }>
+     */
+    public function provideDataForTestNormalize(): iterable
+    {
+        yield [
+            'link.invalid',
+            new Value('link.invalid', 'foo'),
+        ];
+
+        yield [
+            null,
+            new Value(),
+        ];
+    }
+
+    protected function getNormalizer(): ValueNormalizerInterface
+    {
+        return new UrlNormalizer();
+    }
+
+    protected function getValue(): Value
+    {
+        return new Value();
+    }
+}
