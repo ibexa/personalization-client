2d9fc17b5a30e2ed21ded5c70888deaea60a65b4
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:24:17 +0100

    Added MapLocationNormalizer

---
+++ src/bundle/Serializer/Normalizer/FieldType/MapLocationNormalizer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use eZ\Publish\Core\FieldType\MapLocation\Value as MapLocationValue;
+use eZ\Publish\SPI\Exception\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class MapLocationNormalizer implements ValueNormalizerInterface
+{
+    public function normalize(Value $value): ?string
+    {
+        if (!$value instanceof MapLocationValue) {
+            throw new InvalidArgumentType('$value', MapLocationValue::class);
+        }
+
+        return $value->address;
+    }
+
+    public function supportsValue(Value $value): bool
+    {
+        return $value instanceof MapLocationValue;
+    }
+}
