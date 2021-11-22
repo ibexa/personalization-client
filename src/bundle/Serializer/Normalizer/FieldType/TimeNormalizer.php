f74f5a4c8e8124fe7fe253394683310ce9feeb2b
Tomasz Kryszan <tomasz.kryszan@ez.no>
2021-11-22 15:27:50 +0100

    Added TimeNormalizer

---
+++ src/bundle/Serializer/Normalizer/FieldType/TimeNormalizer.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\PersonalizationClient\Serializer\Normalizer\FieldType;
+
+use DateTime;
+use eZ\Publish\Core\FieldType\Time\Value as TimeValue;
+use eZ\Publish\SPI\Exception\InvalidArgumentType;
+use eZ\Publish\SPI\FieldType\Value;
+use Ibexa\Contracts\PersonalizationClient\Serializer\Normalizer\ValueNormalizerInterface;
+
+final class TimeNormalizer implements ValueNormalizerInterface
+{
+    public function normalize(Value $value): ?string
+    {
+        if (!$value instanceof TimeValue) {
+            throw new InvalidArgumentType('$value', TimeValue::class);
+        }
+
+        $time = $value->time;
+        if (null !== $time) {
+            $dateTime = new DateTime("@{$time}");
+
+            return $dateTime->format($value->stringFormat);
+        }
+
+        return null;
+    }
+
+    public function supportsValue(Value $value): bool
+    {
+        return $value instanceof TimeValue;
+    }
+}
